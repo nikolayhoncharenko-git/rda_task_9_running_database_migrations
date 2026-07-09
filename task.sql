9aa9cc2bdd406a7ba36c30ff5d85bd815725e188
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-07-09 13:32:57 +0300

    Solution

--- task.sql
+++ task.sql
@@ -38,3 +38,18 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset nikolay.honcharenko:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+--rollback DROP TABLE Users;
+
+--changeset nikolay.honcharenko:6 labels:0.0.3
+CREATE INDEX EmailIndex 
+ON Users (Email);
+--rollback DROP INDEX EmailIndex ON Users;
